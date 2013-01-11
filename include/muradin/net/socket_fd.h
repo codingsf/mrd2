37632ec2be7af5d629667cb3a238001063c7b681
john chan <cpp.cheen@gmail.com>
2013-01-11 15:15:28 +0800

    add readme file

--- include/muradin/net/socket_fd.h
+++ include/muradin/net/socket_fd.h
@@ -6,8 +6,10 @@ namespace muradin{
 namespace net{
 
 #if defined(PREDEF_OS_WINDOWS)
+	/// SOCKET handle on windows
 	typedef		UINT_PTR		SOCKET_FD;	/* unsigned int  */
 #elif ( defined(PREDEF_OS_LINUX) || defined(PREDEF_PLATFORM_UNIX) )
+	/// SOCKET handle on linux/unix
 	typedef		int				SOCKET_FD;
 #endif
 }
